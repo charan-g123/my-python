72322f19b5a9c4a784849f18c364d4413f671494
charan-g123 <31612456+charan-g123@users.noreply.github.com>
2017-09-14 15:25:16 +0530

    Add files via upload

---
+++ doc.py
@@ -0,0 +1,4 @@
+1)first day of my python class 
+2)which i started to learn an new os called linux 
+3)and started with  basic program like geting r name has output
+4)and learned basic mathematicis like add,sub,divie
