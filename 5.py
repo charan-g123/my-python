79a1e09be7db27e285d6625c2206233f670508e6
charan-g123 <31612456+charan-g123@users.noreply.github.com>
2017-09-28 14:50:21 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -2,13 +2,13 @@
 #guess what this program does???
 import random
 r=random.randint(23,49)#gives random num
-print(r)
+print(r)# display output
 if r<35
-print(r)
+print(r)# display output
 print(":is less than 35")
-elifr==30;
+elifr==30;#gives output less than 35
 print("30 is multiple of 10 and 3 both")
-elif r>=35;
+elif r>=35;#gives output for multiple
 print(r,"is great than 35")
 else:
-	print("your number is:",r)
+	print("your number is:",r)# gives output
